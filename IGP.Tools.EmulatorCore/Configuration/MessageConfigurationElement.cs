2f1a685cb49e4eb5d78b644cd099a33a28b1a1e6
Artem Avdosev <artem.avdosev@gmail.com>
2014-08-01 00:42:53 +0400

    #5 Add configuration elements.

---
+++ IGP.Tools.EmulatorCore/Configuration/MessageConfigurationElement.cs
@@ -0,0 +1,16 @@
+﻿namespace IGP.Tools.EmulatorCore.Configuration
+{
+    using System.Xml.Serialization;
+
+    internal class MessageConfigurationElement
+    {
+        [XmlAttribute]
+        public uint TimeInterval { get; set; }
+
+        [XmlAttribute]
+        public string FormatString { get; set; }
+
+        [XmlElement("ValueSet")]
+        public ValueSetConfigurationElement[] ValuesSets { get; set; }
+    }
+}
---
+++ IGP.Tools.EmulatorCore/Configuration/ValueSetConfigurationElement.cs
@@ -0,0 +1,13 @@
+﻿    namespace IGP.Tools.EmulatorCore.Configuration
+{
+    using System.Xml.Serialization;
+
+    internal class ValueSetConfigurationElement
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        [XmlElement("Value")]
+        public string[] Values { get; set; }
+    }
+}
