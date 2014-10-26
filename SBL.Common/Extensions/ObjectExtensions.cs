6838c003e7b9683ae1c7388a531e0b9be754136f
Artem Avdosev <artem.avdosev@gmail.com>
2014-10-27 01:14:21 +0300

    Eval extension implemented in SBL.Common

---
+++ SBL.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace SBL.Common.Extensions
+{
+    using System;
+
+    public static class ObjectExtensions
+    {
+        public static TResult Eval<T, TResult>(
+            this T obj,
+            Func<T, TResult> func,
+            Func<TResult> defaultProvider = null) where T : class
+        {
+            Contract.ArgumentIsNotNull(func, () => func);
+
+            TResult result = default(TResult);
+            if (obj != null)
+            {
+                result = func(obj);
+            }
+            else if (defaultProvider != null)
+            {
+                result = defaultProvider();
+            }
+
+            return result;
+        }
+    }
+}
