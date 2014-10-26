43c08ca744aa958ef1d4fe3566835e12c24bb8fe
Artem Avdosev <artem.avdosev@gmail.com>
2014-10-26 22:50:31 +0300

    #6: SBL.Common project created

---
+++ SBL.Common/Contracts/Guard.cs
@@ -0,0 +1,13 @@
+﻿namespace SBL.Common.Contracts
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public static class Guard
+    {
+        public static void ArgumentIsNotNull<T>(T value, Expression<Action<T>> ex) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SBL.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace SBL.Common.Extensions
+{
+    using System;
+    using System.IO;
+
+    public static class StringExtensions
+    {
+        public static Stream ToStream(this string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
