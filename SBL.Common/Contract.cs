76ef5fa05b8111dab034f6b6b6e28a70100d4e48
Artem Avdosev <artem.avdosev@gmail.com>
2014-11-01 20:37:18 +0300

    #17 Added [DebuggerStepThrough] annotation for contracts methods and [CallerMemberName] used for exception message.

--- SBL.Common/Contract.cs
+++ SBL.Common/Contract.cs
@@ -1,61 +1,92 @@
 ﻿namespace SBL.Common
 {
     using System;
+    using System.Diagnostics;
     using System.Linq.Expressions;
+    using System.Runtime.CompilerServices;
 
-    // TODO: add caller name
-    // TODO: non browsable by debugger
     public static class Contract
     {
-        public static void ArgumentIsNotNull<T>(T value, Expression<Func<T>> valueName) where T : class
+        [DebuggerStepThrough]
+        public static void ArgumentIsNotNull<T>(
+            T value,
+            Expression<Func<T>> valueName,
+            [CallerMemberName] string caller = "")
+            where T : class
         {
             if (value == null)
             {
                 throw new ArgumentNullException(
-                    string.Format("Argument '{0}' is null", valueName.GetFieldName()));
+                    string.Format(
+                        "Argument '{0}' is null in '{1}' method",
+                        valueName.GetFieldName(),
+                        caller));
             }
         }
 
-        public static void ArgumentSatisfied<T>(T value, Expression<Func<T>> valueName, Func<T, bool> checker)
+        [DebuggerStepThrough]
+        public static void ArgumentSatisfied<T>(
+            T value,
+            Expression<Func<T>> valueName,
+            Func<T, bool> checker,
+            [CallerMemberName] string caller = "")
         {
             if (!checker(value))
             {
                 throw new ArgumentException(
-                    string.Format("Argument '{0}' doesn't satisfied expression", valueName.GetFieldName()));
+                    string.Format(
+                        "Argument '{0}' doesn't satisfied expression in '{1}' method",
+                        valueName.GetFieldName(),
+                        caller));
             }
         }
 
-        public static void IsNotNull<T>(T value, Func<string> messageProvider = null) where T : class
+        [DebuggerStepThrough]
+        public static void IsNotNull<T>(
+            T value,
+            Func<string> messageProvider = null,
+            [CallerMemberName] string caller = "") where T : class
         {
             if (value == null)
             {
-                string message = messageProvider == null ? string.Format("Object is null") : messageProvider();
-
-                throw new ContractException(message);
-            }
-        }
-
-        public static void IsTrue(bool value, Func<string> messageProvider = null)
-        {
-            if (!value)
-            {
                 string message = messageProvider == null ?
-                    string.Format("Expression should be true but was false") :
+                    string.Format("Object is null in '{0}' method", caller) :
                     messageProvider();
 
                 throw new ContractException(message);
             }
         }
 
-        public static void OfType<T>(object value, Func<string> messageProvider = null)
+        [DebuggerStepThrough]
+        public static void IsTrue(
+            bool value,
+            Func<string> messageProvider = null,
+            [CallerMemberName] string caller = "")
+        {
+            if (!value)
+            {
+                string message = messageProvider == null ?
+                    string.Format("Expression should be true but was false in '{0}' method", caller) :
+                    messageProvider();
+
+                throw new ContractException(message);
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void OfType<T>(
+            object value,
+            Func<string> messageProvider = null,
+            [CallerMemberName] string caller = "")
         {
             if (!(value is T))
             {
                 string message = messageProvider == null ?
                     string.Format(
-                        "Object should be of type '{0}' but was '{1}'",
+                        "Object should be of type '{0}' but was '{1}' in '{2}' method",
                         typeof (T).ToString(),
-                        value.GetType().ToString()) :
+                        value.GetType().ToString(),
+                        caller) :
                     messageProvider();
 
                 throw new ContractException(message);
