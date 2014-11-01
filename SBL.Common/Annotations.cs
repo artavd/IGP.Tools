473e71e46633de265616e70eb1669c4e2a686e20
Artem Avdosev <artem.avdosev@gmail.com>
2014-11-01 20:13:02 +0300

    #17 Code annotations [NotNull] and [CanBeNull] added

---
+++ SBL.Common/Annotations.cs
@@ -0,0 +1,18 @@
+﻿namespace SBL.Common.Annotations
+{
+    using System;
+
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property |
+        AttributeTargets.Delegate | AttributeTargets.Field,
+        AllowMultiple = false,
+        Inherited = true)]
+    public sealed class CanBeNullAttribute : Attribute { }
+
+    [AttributeUsage(
+        AttributeTargets.Method | AttributeTargets.Parameter | AttributeTargets.Property |
+        AttributeTargets.Delegate | AttributeTargets.Field,
+        AllowMultiple = false,
+        Inherited = true)]
+    public sealed class NotNullAttribute : Attribute { }
+}
