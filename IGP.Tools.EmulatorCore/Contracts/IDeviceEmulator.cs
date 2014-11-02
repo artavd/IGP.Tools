765f2c5a62c40be7323e5591b10eb8faf2a81e64
Artem Avdosev <artem.avdosev@gmail.com>
2014-11-02 14:37:46 +0300

    #25 Added IDeviceEmulator interface to control emulation process.

---
+++ IGP.Tools.EmulatorCore/Contracts/IDeviceEmulator.cs
@@ -0,0 +1,13 @@
+﻿namespace IGP.Tools.EmulatorCore
+{
+    public interface IDeviceEmulator
+    {
+        bool IsTimeIncluded { get; set; }
+
+        bool IsStarted { get; }
+
+        void Start();
+
+        void Stop();
+    }
+}
