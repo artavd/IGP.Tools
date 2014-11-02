7102a36a96bbbf11a9a5f275acc737011a4a0527
Artem Avdosev <artem.avdosev@gmail.com>
2014-11-02 14:39:16 +0300

    #25 IDeviceEmulator implemented

--- IGP.Tools.EmulatorCore/Implementation/DeviceEmulator.cs
+++ IGP.Tools.EmulatorCore/Implementation/DeviceEmulator.cs
@@ -2,17 +2,36 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reactive.Linq;
+    using System.Reactive.Subjects;
     using SBL.Common;
     using SBL.Common.Annotations;
+    using SBL.Common.Extensions;
 
-    internal sealed class DeviceEmulator : IDevice
+    internal sealed class DeviceEmulator : IDevice, IDeviceEmulator
     {
+        private readonly BehaviorSubject<bool> _switcher = new BehaviorSubject<bool>(true);
+
         public IList<IObservable<byte[]>> Messages { get; private set; }
 
         public string Name { get; private set; }
 
         public bool IsTimeIncluded { get; set; }
+
+        public bool IsStarted { get; private set; }
+
+        public void Start()
+        {
+            _switcher.OnNext(true);
+            IsStarted = true;
+        }
+
+        public void Stop()
+        {
+            _switcher.OnNext(false);
+            IsStarted = false;
+        }
 
         public DeviceEmulator(
             [NotNull] string name,
@@ -25,16 +44,23 @@
 
             Name = name;
             IsTimeIncluded = false;
+            IsStarted = true;
 
             Messages = new List<IObservable<byte[]>>();
-            foreach (var mp in messageProviders)
-            {
-                var messageFeed = Observable
-                    .Interval(mp.Interval)
-                    .Select(_ => encoder.Encode(mp.GetNextMessage()));
 
-                Messages.Add(messageFeed);
-            }
+            messageProviders
+                .Select(x => Observable
+                    .Interval(x.Interval)
+                    .Pausable(_switcher)
+                    .Select(n => encoder.Encode(Decorate(x.GetNextMessage(), n))))
+                .Foreach(Messages.Add);
+        }
+
+        private string Decorate(string source, long number)
+        {
+            return IsTimeIncluded ?
+                string.Format("{0} | {1:D4} | {2}", DateTime.Now.ToLocalTime(), number, source) :
+                source;
         }
 
         #region Disposing
@@ -51,7 +77,7 @@
 
         private void Dispose(bool disposing)
         {
-            if (disposing) { }
+            Messages.Foreach(m => m.DisposeIfPossible());
         }
         #endregion
     }
---
+++ SBL.Common/Extensions/ObservableExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace SBL.Common.Extensions
+{
+    using System;
+    using System.Reactive.Linq;
+    using SBL.Common.Annotations;
+
+    public static class ObservableExtensions
+    {
+        [NotNull]
+        public static IObservable<T> Pausable<T>(
+            [NotNull] this IObservable<T> observable,
+            [NotNull] IObservable<bool> switcher)
+        {
+            Contract.ArgumentIsNotNull(observable, () => observable);
+            Contract.ArgumentIsNotNull(switcher, () => switcher);
+
+            return switcher
+                .DistinctUntilChanged()
+                .Where(on => on)
+                .SelectMany(observable.TakeUntil(switcher.Where(on => !on)));
+        }
+    }
+}
