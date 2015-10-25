cc7f089c75961dc7b46bb5cd029830146333a27e
Artem Avdosev <artem.avdosev@gmail.com>
2015-10-25 20:04:42 +0300

    Added SBL.WPF.Controls project and skeleton for GlowChromeBehavior [#44]

---
+++ SBL.WPF.Controls/Glow/GlowChromeBehavior.cs
@@ -0,0 +1,10 @@
+﻿namespace SBL.WPF.Controls
+{
+    using System.Windows;
+    using System.Windows.Interactivity;
+
+    public sealed class GlowChromeBehavior : Behavior<Window>
+    {
+         
+    }
+}
---
+++ SBL.WPF.Controls/Glow/GlowWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace SBL.WPF.Controls
+{
+    using System.Windows;
+
+    internal sealed class GlowWindow : Window
+    {
+        static GlowWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GlowWindow), new FrameworkPropertyMetadata(typeof(GlowWindow)));
+        }
+    }
+}
